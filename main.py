84fe12b3c1cd9ac5ff72535d5ad1951da4c91619
jackwang <jack4shihong@gmail.com>
2025-02-26 15:44:47 +0800

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+DEC (Unsupervised Deep Embedding for Clustering Analysis) 主程序
+
+    @Time    : 2025/02/25
+    @Author  : JackWang
+    @File    : main.py
+    @IDE     : VsCode
+"""
+
+# Standard Library
+import datetime
+from pathlib import Path
+
+# Third-Party Library
+
+# Torch Library
+import torch
+
+# My Library
