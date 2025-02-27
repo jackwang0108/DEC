43749b6f560932d460f14e498bea79f755d7e905
jackwang <jack4shihong@gmail.com>
2025-02-27 19:08:39 +0800

    refactor add_layer by context manager

--- network.py
+++ network.py
@@ -9,6 +9,7 @@ network 定义了DEC网络的结构
 
 # Standard Library
 from typing import Literal
+from contextlib import contextmanager
 
 # Third-Party Library
 
@@ -24,12 +25,17 @@ class DEC(nn.Module):
         super(DEC, self).__init__()
 
         self.auto_encoders = nn.ModuleDict()
-        self.non_linear_mapping = nn.Sequential(nn.Identity())
+        self.final_encoder = nn.Sequential(nn.Identity())
+        self.final_decoder = nn.Sequential(nn.Identity())
 
-    def add_layer(
+        self.current_auto_encoder = None
+
+    @contextmanager
+    def train_new_layer(
         self,
         in_dim: int,
         hidden_dim: int,
+        freeze: bool = True,
         # Sec.3.2 Parameter initialization: ... except for g2 of the first pair (it needs to reconstruct input data that may have positive and negative values, such as zero-mean images) and g1 of the last pair (so the final data embedding retains full information ...
         which_pair: Literal["first", "last", "mid"] = True,
         device: torch.device = torch.device("cpu"),
@@ -51,24 +57,36 @@ class DEC(nn.Module):
         nn.init.normal_(encoder[1].weight, mean=0, std=0.01)
         nn.init.normal_(decoder[1].weight, mean=0, std=0.01)
 
-        self.non_linear_mapping.add_module(
-            f"encoder_{len(self.non_linear_mapping)}", encoder
-        )
         self.auto_encoders.add_module(
             name=f"autoencoder {len(self.auto_encoders) + 1}",
-            module=nn.Sequential(encoder, decoder),
+            module=(cae := nn.Sequential(encoder, decoder)),
         )
+
+        self.current_auto_encoder = cae
+
+        yield self
+
+        # freeze the weights of the trained encoder
+        if freeze:
+            for param in encoder.parameters():
+                param.requires_grad = False
+
+            for param in decoder.parameters():
+                param.requires_grad = False
+
+        self.final_encoder.append(encoder)
+        self.final_decoder.insert(0, decoder)
 
 
 if __name__ == "__main__":
 
     dec = DEC()
-    dec.add_layer(784, 500, which_pair="first")
-    dec.add_layer(500, 500, which_pair="mid")
-    dec.add_layer(500, 2000, which_pair="mid")
-    dec.add_layer(2000, 10, which_pair="last")
+    dec.train_new_layer(784, 500, which_pair="first")
+    dec.train_new_layer(500, 500, which_pair="mid")
+    dec.train_new_layer(500, 2000, which_pair="mid")
+    dec.train_new_layer(2000, 10, which_pair="last")
     print(dec)
 
     image = torch.rand(4, 784)
-    print(dec.non_linear_mapping(image).shape)
+    print(dec.final_encoder(image).shape)
     print(dec.auto_encoders["autoencoder 0"](image).shape)
